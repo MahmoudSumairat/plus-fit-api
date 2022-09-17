645106da17df26ad3f4775f1a2e949f512f41ef7
Mahmoud Bassam <mahmoudbassam@dev-malsmairat.home>
2022-09-18 00:40:37 +0300

    deployment

---
+++ src/config/mail.js
@@ -0,0 +1,11 @@
+exports.mailConfig = {
+  EMAIL: "mahmoud.business98@gmail.com",
+  PASSWORD: "zabiqcslkijzpiad",
+};
+
+exports.mailOptions = {
+  from: "mahmoud@pf.com",
+  to: "roys.msc2017@gmail.com",
+  subject: "Plus Fit visit report",
+  text: "Hey Boss, someone has visited your plus fit app!",
+};
