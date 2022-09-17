46cf644d7866f3c89c8a07f86f659892017718e2
Mahmoud Bassam <mahmoudbassam@dev-malsmairat.home>
2022-09-17 23:22:41 +0300

    added env variables to npm

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
+const isProd = process.env.NODE_ENV === "prod";
 
 const dbInitConnectionConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: isProd ? process.env.DB_HOST : "localhost",
+  user: isProd ? process.env.DB_USER : "root",
+  password: isProd ? process.env.DB_PASSWORD : "Mahmoud@1998",
   connectionLimit: process.env.DB_CONNECTION_LIMIT,
   multipleStatements: true,
 };
