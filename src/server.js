f8a8d8e30af7af710c88bd9444ee5f21ab63aad4
Mahmoud Bassam <mahmoudbassam@dev-malsmairat.home>
2022-09-18 00:34:10 +0300

    added production url

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const fileupload = require("express-fileupload");
 const cors = require("cors");
 const { format } = require("mysql");
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://plus-fit.herokuapp.com/" }));
 
 app.use(fileupload());
 
